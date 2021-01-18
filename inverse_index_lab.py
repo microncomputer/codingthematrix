13c3d23d3e1d4870dc46d9c4b5211e3151743221
microncomputer <hazel2@pdx.edu>
2021-01-18 15:00:53 -0800

    first commit

---
+++ inverse_index_lab.py
@@ -0,0 +1,81 @@
+# version code 80e56511a793+
+# Please fill out this stencil and submit using the provided submission script.
+
+
+
+
+
+## 1: (Task 0.6.2) Movie Review
+## Task 1
+def movie_review(name):
+    """
+    Input: the name of a movie
+    Output: a string (one of the review options), selected at random using randint
+    """
+    from random import randint
+    return ['Simply Shocking', 'See It!', 'All Star Cast!'][randint(0,3)]
+
+
+
+## 2: (Task 0.6.6) Make Inverse Index
+def makeInverseIndex(strlist):
+    return {subword:{x for (x, str) in enumerate(strlist) if subword in str.split()} for str in strlist for subword in str.split()} 
+
+    """
+    Input: a list of documents as strings
+    Output: a dictionary that maps each word in any document to the set consisting of the
+            document ids (ie, the index in the strlist) for all documents containing the word.
+    Distinguish between an occurence of a string (e.g. "use") in the document as a word
+    (surrounded by spaces), and an occurence of the string as a substring of a word (e.g. "because").
+    Only the former should be represented in the inverse index.
+    Feel free to use a loop instead of a comprehension.
+
+    Example:
+    >>> makeInverseIndex(['hello world','hello','hello cat','hellolot of cats']) == {'hello': {0, 1, 2}, 'cat': {2}, 'of': {3}, 'world': {0}, 'cats': {3}, 'hellolot': {3}}
+    True
+    """
+
+
+
+## 3: (Task 0.6.7) Or Search
+def orSearch(inverseIndex, query):
+
+    return {docnum for sublist in inverseIndex[word] for word in query for docnum in sublist}
+    """
+    Input: an inverse index, as created by makeInverseIndex, and a list of words to query
+    Output: the set of document ids that contain _any_ of the specified words
+    Feel free to use a loop instead of a comprehension.
+    
+    >>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    >>> orSearch(idx, ['Bach','the'])
+    {0, 2, 3, 4, 5}
+    >>> orSearch(idx, ['Johann', 'Carl'])
+    {0, 2, 3, 4, 5}
+    >>> orSearch(idx, ['Johann', 'Bach', 'Sebastian'])
+    {0, 2, 3, 4, 5}
+    >>> idx == makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    True
+    """
+
+
+
+## 4: (Task 0.6.8) And Search
+def andSearch(inverseIndex, query):
+    return set.intersection(*[inverseIndex[query[i]] for i in range(len(query))])
+    """
+    Input: an inverse index, as created by makeInverseIndex, and a list of words to query
+    Output: the set of all document ids that contain _all_ of the specified words
+    Feel free to use a loop instead of a comprehension.
+
+    >>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    >>> andSearch(idx, ['Johann', 'the'])
+    {2, 3}
+    >>> andSearch(idx, ['Johann', 'Bach'])
+    {0, 4}
+    >>> andSearch(idx, ['Johann', 'Bach', 'Sebastian'])
+    {0}
+    >>> idx == makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    True
+    """
+    pass
+
