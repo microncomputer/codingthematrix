ed392ed959c138ba951558a2f7375b413ea32125
microncomputer <hazel2@pdx.edu>
2021-11-02 16:15:38 -0700

    started vector problems

--- 02_TheVector/The_Vector_problems.py
+++ 02_TheVector/The_Vector_problems.py
@@ -12,25 +12,53 @@ from Tools.GF2 import one
 #Please express each answer as a list of numbers
 p1_v = [-1, 3]
 p1_u = [0, 4]
-p1_v_plus_u = [...]
-p1_v_minus_u = [...]
-p1_three_v_minus_two_u = [...]
+p1_v_plus_u = [p1_v[i]+p1_u[i] for i in range(len(p1_v))]
+p1_v_minus_u = [p1_v[i]-p1_u[i] for i in range(len(p1_v))]
+p1_three_v_minus_two_u = [3*p1_v[i]-2*p1_u[i] for i in range(len(p1_v))]
 
 
 
 ## 2: (Problem 2.14.2) Vector Addition Practice 2
 p2_u = [-1,  1, 1]
 p2_v = [ 2, -1, 5]
-p2_v_plus_u = [...]
-p2_v_minus_u = [...]
-p2_two_v_minus_u = [...]
-p2_v_plus_two_u = [...]
+p2_v_plus_u = [p2_v[i]+p2_u[i] for i in range(len(p2_v))]
+p2_v_minus_u = [p2_v[i]-p2_u[i] for i in range(len(p2_v))]
+p2_two_v_minus_u = [2*p2_v[i]-p2_u[i] for i in range(len(p2_v))]
+p2_v_plus_two_u = [2*p2_u[i]+p2_v[i] for i in range(len(p2_v))]
 
 
 
 ## 3: (Problem 2.14.3) Vector Addition Practice 3
 # Write your answer using GF2's one instead of the number 1
-p3_vector_sum_1 = [...]
+'''
+find subset of a-f that sums to u1 and that sums to u2
+u1 = 0010010
+u2 = 0100010
+'''
+a = [one,one,0,0,0,0,0]
+d = [0,0,0,one,one,0,0]
+b = [0,one,one,0,0,0,0]
+e = [0,0,0,0,one,one,0]
+c = [0,0,one,one,0,0,0]
+f = [0,0,0,0,0,one,one]
+
+def vector_sum_subset(sumVector, vectorSet):
+    """
+    I will always admit when I did not do my own work and this is a moment where I
+    must admit I found the next algorithm for making a powerset on stack exchange
+    in a moment of low energy and escapism. hey, we all do it.
+    the part I did not understand was the "if i&1<<k" which I assumed was some kind of bit shifting
+    so I will try to explain it for my own understanding here.
+    i&1 is bitwise AND, not logical AND. it means if whatever i evaluates to in binary
+    has a 1 in the 2^0 place, the number i is odd. i&1 means true if i is odd.
+
+    
+    """
+
+    subsets = [[vectorSet[k] for k in range(len(vectorSet)) if i&1<<k] for i in range(2**(len(vectorSet)))]
+    return subsets
+
+p3_vector_sum_1 = []
 p3_vector_sum_2 = [...]
 
 
